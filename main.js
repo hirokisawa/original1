5994b08de37ed05fb59bed0233d64268c4a4005f
Ubuntu <ubuntu@ip-172-31-36-24.ap-northeast-1.compute.internal>
2021-07-23 11:21:50 +0000

    init Laravel Project

---
+++ main.js
@@ -0,0 +1,14 @@
+$('body').fadeIn(3000, function() {
+  
+});
+
+
+$(function(){
+  $('.single-item').slick({
+    accessibility: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    fade: true,
+  });
+});
